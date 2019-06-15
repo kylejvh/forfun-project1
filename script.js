08f7957982575dc8a00e006e4f244636fa8310b9
kylejvh <kyvhy66@gmail.com>
2019-06-15 00:07:09 -0400

    adding starting project

---
+++ script.js
@@ -0,0 +1,10 @@
+var audio = new Audio('out.mp3'); 
+audio.onload = audio.play();
+
+
+// addEventListener.onkeydown = s, i, l, v, i, o
+
+
+
+// document.addEventListener('keydown', logKey);
+    // if { logKey = s}
