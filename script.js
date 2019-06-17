eb70407da74140c93798e6f061eeb16047ebc737
kylejvh <kyvhy66@gmail.com>
2019-06-17 00:39:50 -0400

    added sound effects, form question+validation logic, and initial animation code

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
-var audio = new Audio('out.mp3'); 
-audio.onload = audio.play();
+let audioInitial = new Audio('out.mp3'); 
+audioInitial.onload = audioInitial.play();
+
+let audioSubmitTrue = new Audio('whattayahearwhattayasay.mp3');
+let audioSubmitFalse = new Audio('canihelpyou.mp3');
+
+function validateName() {
+   let nameCheck = document.getElementById("testform").value;
+    if (nameCheck == "Silvio") {
+        audioSubmitTrue.play();
+        alert("pass!");
+    }   else {
+        audioSubmitFalse.play();
+        alert("No disrespect T, maybe you oughta try again."); // change alert to trigger a picture to show,
+        return false;             // maybe change the message.
+    }
+}
+
+
+
+ /* let nameCheck = function validateName() {  // rename this function?
+    if  (document.getElementById("nameform") === "Tony") {
+        audioTony.play();
+    }   else { alert("No, you're Tony");
+   }
+} */
 
 
 // addEventListener.onkeydown = s, i, l, v, i, o
@@ -7,7 +31,7 @@ audio.onload = audio.play();
 
 
 
-/* when the following keys are pressed, reveal all of the buttons. 
+// when the following keys are pressed, reveal all of the buttons.
 
 // document.addEventListener('keydown', logKey);
     // if { logKey = s}
