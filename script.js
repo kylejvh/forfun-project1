101e854c525ded82b9c62fd1ddd6b0759c4869fb
kylejvh <kyvhy66@gmail.com>
2019-07-04 21:12:13 -0400

    Removed bootstrap. Overhauled styling. Implemented Flexbox layout and initial media query breakpoint. Organized and implemented soundfiles and sound buttons.

--- script.js
+++ script.js
@@ -1,28 +1,42 @@
+function soundSwitcher(soundfile) {
+  document.getElementById('initialAudio').src = soundfile;
+  document.getElementById('initialAudio').play();
+  }
+  
+function classToggle() {
+  
+  masterDiv.classList.toggle("activeDiv");
+}
+  
 
 
 
 
 
 let coll = document.getElementsByClassName("collapseBtn");
+let masterDiv = document.querySelector("#tonyLayout");
+let masterDiv1 = document.querySelector("#paulieLayout");
+let masterDiv2 = document.querySelector("#juniorLayout");
 let i;
 
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
+   /* masterDiv.classList.toggle("tonyMasterDiv");
+    masterDiv1.classList.toggle("tonyMasterDiv");
+    masterDiv2.classList.toggle("tonyMasterDiv"); */
+
     let content = this.nextElementSibling;
-    if (content.style.display == "block") {
+    if (content.style.display == "flex") {
       content.style.display = "none";
     } else {
-      content.style.display = "block";
+      content.style.display = "flex";
     }
   });
 }
 
 
-function soundSwitcher(soundfile) {
-document.getElementById('initialAudio').src = soundfile;
-document.getElementById('initialAudio').play();
-}
+;
 
 
 
@@ -36,20 +50,17 @@ document.getElementById('initialAudio').play();
 
 function validateName() {
    let nameCheck = document.getElementById('testform').value;
-    if (nameCheck == 'Silvio') {
+    if (nameCheck === 'Silvio') {
         document.getElementById('initialAudio').src = "whattayahearwhattayasay.mp3";
         document.getElementById('initialAudio').play();
+        setTimeout(function(){ alert("success!"); }, 250);
          // change this alert...
-        location.replace("soundboard.html");
+         setTimeout(function(){ location.replace("soundboard.html"); }, 2000);
         // call function here that hides img and shows list of sounds
     }   else {
         document.getElementById('initialAudio').src = "canihelpyou.mp3";
         document.getElementById('initialAudio').play();
-        alert('No disrespect T, maybe you oughta try again.'); // change alert to trigger a picture to show  
+        setTimeout(function(){ alert('No disrespect T, maybe you oughta try again.'); }, 250); // change alert to trigger a picture to show  
         // maybe change the message.
     }
 }
-
-
-
-
