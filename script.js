603d666a9f19cf41259d2969f3078b129b118977
kylejvh <kyvhy66@gmail.com>
2019-07-05 12:40:13 -0400

    Fixed logic of validateName function to accept 2 case insensitive name variations on form submit.

--- script.js
+++ script.js
@@ -5,29 +5,14 @@ function soundSwitcher(soundfile) {
   document.getElementById('initialAudio').play();
   }
   
-function classToggle() {
-  
-  masterDiv.classList.toggle("activeDiv");
-}
-  
-
-
-
 
 
 let coll = document.getElementsByClassName("collapseBtn");
-let masterDiv = document.querySelector("#tonyLayout");
-let masterDiv1 = document.querySelector("#paulieLayout");
-let masterDiv2 = document.querySelector("#juniorLayout");
 let i;
 
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
-   /* masterDiv.classList.toggle("tonyMasterDiv");
-    masterDiv1.classList.toggle("tonyMasterDiv");
-    masterDiv2.classList.toggle("tonyMasterDiv"); */
-
     let content = this.nextElementSibling;
     if (content.style.display == "flex") {
       content.style.display = "none";
@@ -38,7 +23,6 @@ for (i = 0; i < coll.length; i++) {
 }
 
 
-;
 
 
 
@@ -46,19 +30,16 @@ for (i = 0; i < coll.length; i++) {
 
 
 
-// the simplest way to do this is to add 
-// a class to the animation to trigger it?
 
 
 function validateName() {
-   let nameCheck = document.getElementById('testform').value;
-    if (nameCheck === 'Silvio') {
+   let nameCheck = document.getElementById('testform').value.toLowerCase();
+    if (nameCheck == 'sil' || nameCheck == 'silvio')  { 
         document.getElementById('initialAudio').src = "whattayahearwhattayasay.mp3";
         document.getElementById('initialAudio').play();
         setTimeout(function(){ alert("success!"); }, 250);
          // change this alert...
          setTimeout(function(){ location.replace("soundboard.html"); }, 2000);
-        // call function here that hides img and shows list of sounds
     }   else {
         document.getElementById('initialAudio').src = "canihelpyou.mp3";
         document.getElementById('initialAudio').play();
